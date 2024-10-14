0e12f450deb4befb86a97e8f297684590eaf503d
hungnguyen988 <hungnguuen988@gmail.com>
2024-10-14 11:46:35 +0700

    nop_bai

--- ss1.student_management.sql
+++ ss1.student_management.sql
@@ -11,10 +11,11 @@ country varchar(50)
 
 
 
-create table class(
-id int ,
+create table if not exists class(
+id int primary key auto_increment,
 name varchar(200)
 );
+
 
 create table teacher(
 id int ,
@@ -22,3 +23,19 @@ name varchar(200),
 age int ,
 country varchar(50)
 );
+
+insert into  student values( 1,"hung",26,"vietnam");
+select * from student;
+
+insert into student 
+values(2,"john",24,"my"),
+(3,"phuong",25,"vietnam");
+
+alter table student modify id int auto_increment;
+alter table class add column(quantity int);
+
+update student set name = "minh" where id = 3;
+
+delete from student where id = 2;
+
+
