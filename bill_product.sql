e220fcd1fe1811094e396acfb2566ac1a2ef34f0
hungnguyen988 <hungnguuen988@gmail.com>
2024-10-16 19:07:48 +0700

    nop_bai

--- bill_product.sql
+++ bill_product.sql
@@ -47,3 +47,44 @@ values('may giat',3),
 ('dieu hoa',7),
 ('quat',1),
 ('bep dien',2);
+
+insert into order_detail
+values(1,1,3),
+(3,1,7),
+(4,1,2),
+(1,2,1),
+(1,3,8),
+(5,2,4),
+(3,2,3);
+
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng `order`
+
+select `order`.id,`order`.date_order,`order`.total_price
+from `order`;
+
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+
+select customer.name,product.name
+from `order`
+join customer on customer.id = `order`.customer_id
+join order_detail on `order`.id = order_detail.order_id
+join product on product.id = order_detail.product_id
+group by customer.name,product.name
+order by customer.name asc
+;
+
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+
+select customer.name
+from customer
+left join `order` on customer.id = `order`.customer_id
+where `order`.customer_id is null;
+
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
+
+select o.id,o.date_order,sum(p.price*od.quantity) as total_money
+from `order` o
+join order_detail od on o.id = od.order_id
+join product p on p.id = od.product_id
+group by o.id,o.date_order;
+
