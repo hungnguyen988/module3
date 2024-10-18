5842aa79f5ee3d2d4348de307f277180288b10e0
hungnguyen988 <hungnguuen988@gmail.com>
2024-10-18 14:32:50 +0700

    nop_bai

--- case_study.sql
+++ case_study.sql
@@ -283,3 +283,7 @@ group by dv.ma;
  group by hd.ma_dich_vu;
  
  -- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau
+ 
+ select khach_hang.ho_ten
+ from khach_hang 
+ group by khach_hang.ho_ten;
