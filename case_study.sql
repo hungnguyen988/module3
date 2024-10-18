23d38c509f4fdd89ad7d8f0da14e3b58247d4d8b
hungnguyen988 <hungnguuen988@gmail.com>
2024-10-18 16:44:23 +0700

    nop_bai

--- case_study.sql
+++ case_study.sql
@@ -370,6 +370,13 @@ group by dv.ma;
  group by month(ngay_lam_hop_dong)
  order by ngay_lam_hop_dong asc;
  
- 
+ -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+ -- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem 
+ -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+select hd.ma as ma_hop_dong, hd.ngay_lam_hop_dong as ngay_lam_hop_dong, hd.ngay_ket_thuc as ngay_ket_thuc, hd.tien_dat_coc as tien_dat_coc, ifnull(sum(so_luong),0)  as tong_so_dich_vu_di_kem 
+from hop_dong hd
+left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma
+group by hd.ma ; 
  
  
