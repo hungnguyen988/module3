6a4c964fc941551262ca174d5213c5e3d0e934ca
hungnguyen988 <hungnguuen988@gmail.com>
2024-10-22 09:08:51 +0700

    nop_bai

--- case_study.sql
+++ case_study.sql
@@ -378,4 +378,11 @@ from hop_dong hd
 left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma
 group by hd.ma ; 
  
+ -- 20.Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, thông tin hiển thị bao gồm 
+ -- id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
  
+ select nv.ma,nv.ho_ten,nv.email,nv.so_dien_thoai,nv.ngay_sinh,nv.dia_chi
+ from nhan_vien nv
+union
+select kh.ma,kh.ho_ten,kh.email,kh.so_dien_thoai,kh.ngay_sinh,kh.dia_chi
+from khach_hang kh;
